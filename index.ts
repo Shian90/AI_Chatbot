8d8ca707f938b6d753a0737301e427288f741a9e
Shian90 <shiankhan90@gmail.com>
2025-10-05 00:02:20 +0600

    Run backend and frontend together.

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!!");
+import concurrently from "concurrently";
+
+concurrently([
+  {
+    name: "Server",
+    command: "bun run dev",
+    cwd: "packages/server",
+    prefixColor: "red",
+  },
+  {
+    name: "Client",
+    command: "bun run dev",
+    cwd: "packages/client",
+    prefixColor: "green",
+  },
+]);
