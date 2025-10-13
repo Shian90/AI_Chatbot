ac3739dfe1b31caf94f488b32df1749800a14f49
Shian90 <shiankhan90@gmail.com>
2025-10-14 02:21:33 +0600

    Modular Code: Extracting repository layer.

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express";
 
 import OpenAI from "openai";
+import { chatHistoryRepository, type ChatMessage } from "./repositories/chat.repository";
 import dotenv from "dotenv";
 import express from "express";
 import z from "zod";
@@ -13,66 +14,44 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("This is bun server on root endpoint!!");
-});
-
 app.listen(port, () => {
   console.log(`The server is listening on http://localhost:${port}`);
 });
 
-interface ChatMessage {
-  response_id?: string;
-  role: "system" | "user" | "assistant";
-  content: string;
-}
+app.get("/", (req: Request, res: Response) => {
+  res.send("This is bun server on root endpoint!!");
+});
 
-const MAX_HISTORY = 20;
-let chatThreadToHistory = new Map<string, ChatMessage[]>();
-
-const client = new OpenAI({
+const client: OpenAI = new OpenAI({
   baseURL: "https://router.huggingface.co/v1",
   apiKey: process.env.HF_TOKEN,
 });
 
 const remoteModelChat = async (chatThreadId: string, prompt: string) => {
-  let chatHistory: ChatMessage[] | undefined =
-    chatThreadToHistory.get(chatThreadId);
-
-  if (!chatHistory) {
-    chatHistory = [];
-    chatThreadToHistory.set(chatThreadId, chatHistory);
-  }
-
-  chatHistory.push({ role: "user", content: prompt });
-
-  const response = await client.chat.completions.create({
-    model: "google/gemma-2-2b-it",
-    messages: chatHistory,
-    max_tokens: 50,
+  chatHistoryRepository.addChatMessageToHistory(chatThreadId, {
+    role: "user",
+    content: prompt,
   });
 
-  const reply: ChatMessage = {
+  const response: OpenAI.Chat.Completions.ChatCompletion = await client.chat.completions.create({
+    model: "google/gemma-2-2b-it",
+    messages: chatHistoryRepository.getChatHistory(chatThreadId),
+    max_tokens: 100,
+  });
+
+  const modelReplyMessage: ChatMessage = {
     response_id: response.id,
-    role: response.choices[0].message.role,
-    content: response.choices[0].message.content,
+    role: response.choices[0]?.message.role ?? "assistant",
+    content: response.choices[0]?.message.content ?? "",
   };
 
-  chatHistory.push(reply);
+  chatHistoryRepository.addChatMessageToHistory(chatThreadId, modelReplyMessage);
 
-  if (chatHistory.length > MAX_HISTORY) {
-    chatHistory.splice(0, chatHistory.length - MAX_HISTORY);
-  }
-
-  return reply.content;
+  return modelReplyMessage.content;
 };
 
 const chatReqSchema = z.object({
-  prompt: z
-    .string()
-    .trim()
-    .min(1, "Prompt is required.")
-    .max(1000, "Prompt is too long! (Max 1000 characters)."),
+  prompt: z.string().trim().min(1, "Prompt is required.").max(1000, "Prompt is too long! (Max 1000 characters)."),
   chatThreadId: z.uuid(),
 });
 
---
+++ packages/server/repositories/chat.repository.ts
@@ -0,0 +1,33 @@
+export interface ChatMessage {
+  response_id?: string;
+  role: "system" | "user" | "assistant";
+  content: string;
+}
+
+const MAX_HISTORY = 20;
+let chatThreadToHistory = new Map<string, ChatMessage[]>();
+
+class ChatHistoryRepository {
+  getChatHistory(chatThreadId: string): ChatMessage[] {
+    let chatHistory: ChatMessage[] | undefined = chatThreadToHistory.get(chatThreadId);
+
+    if (!chatHistory) {
+      chatHistory = [];
+      chatThreadToHistory.set(chatThreadId, chatHistory);
+    }
+
+    return chatHistory;
+  }
+
+  addChatMessageToHistory(chatThreadId: string, chatMessage: ChatMessage): void {
+    let chatHistory: ChatMessage[] = this.getChatHistory(chatThreadId);
+
+    chatHistory.push(chatMessage);
+
+    if (chatHistory.length > MAX_HISTORY) {
+      chatHistory.splice(0, chatHistory.length - MAX_HISTORY);
+    }
+  }
+}
+
+export const chatHistoryRepository = new ChatHistoryRepository();
